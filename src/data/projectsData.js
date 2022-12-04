bead06f05f208c53616c643b634f45831700e118
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-12-04 18:10:29 +0000

    Update Skills & Projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -6,160 +6,160 @@ import {
   SiFramer,
   SiTailwindcss,
   SiFirebase,
-} from 'react-icons/si';
-import CVMaker from '../images/cv-maker.png';
-import TravelLandingPage from '../images/travel-landing-page.png';
-import GitHubSearch from '../images/github-search.png';
-import RentACarLandingPage from '../images/rent-a-car-landing-page.png';
-import DogWiki from '../images/dog-wiki-app.vercel.app_.png';
-import EcommerceApp from '../images/Ecommerce_App.png';
+} from "react-icons/si";
+import CVMaker from "../images/cv-maker.png";
+import TravelLandingPage from "../images/travel-landing-page.png";
+import GitHubSearch from "../images/github-search.png";
+import RentACarLandingPage from "../images/rent-a-car-landing-page.png";
+import DogWiki from "../images/dog-wiki-app.vercel.app_.png";
+import EcommerceApp from "../images/Ecommerce_App.png";
 
 export const projectsData = [
   {
-    title: 'CV Maker',
+    title: "Ecommerce",
     skills: [
       {
-        text: 'React',
-        img: <SiReact />,
-      },
-      { text: 'JavaScript', img: <SiJavascript /> },
-      { text: 'CSS Modules', img: <SiCss3 /> },
-    ],
-    description:
-      "A CV Maker app that updates/changes the CV when you type your information out. With this project I've worked mostly with a large state management and controlled inputs.",
-    demoURL: 'https://cv-makerapp.vercel.app/',
-    codeURL: 'https://github.com/TiagoJacintoDev/cv-maker',
-    img: CVMaker,
-  },
-  {
-    title: 'Travel Landing Page',
-    skills: [
-      {
-        text: 'React',
+        text: "React",
         img: <SiReact />,
       },
       {
-        text: 'JavaScript',
+        text: "Javascript",
         img: <SiJavascript />,
       },
       {
-        text: 'styled-components',
-        img: <SiStyledcomponents />,
+        text: "Firebase",
+        img: <SiFirebase />,
+      },
+      {
+        text: "Tailwind CSS",
+        img: <SiTailwindcss />,
+      },
+      {
+        text: "React Query",
+      },
+      {
+        text: "Axios",
       },
     ],
     description:
-      'This project is a travel agency landing page with a mobile first design. My main focus for this project was to have a visually appealing page with responsive functionality. ',
-    demoURL: 'https://travel-landing-pageapp.vercel.app/',
-    codeURL: 'https://github.com/TiagoJacintoDev/travel-landing-page',
-    img: TravelLandingPage,
+      "This application was inspired in the website KuantoKusta. This was a large project that taught me what are the pain points of building and scaling a large app.",
+    demoURL: "https://ecommerce-tiago.vercel.app/",
+    codeURL: "https://github.com/TiagoJacintoDev/ecommerce",
+    img: EcommerceApp,
   },
   {
-    title: 'GitHub Search',
+    title: "GitHub Search",
     skills: [
       {
-        text: 'React',
+        text: "React",
         img: <SiReact />,
       },
       {
-        text: 'JavaScript',
+        text: "JavaScript",
         img: <SiJavascript />,
       },
       {
-        text: 'CSS',
+        text: "CSS",
         img: <SiCss3 />,
       },
     ],
     description:
       "In this app I've worked with the GitHub Search API to make a search application with the API. With this application I've improved my skills with fetching data from APIs, changing and filtering state, debouncing functions and pagination",
-    demoURL: 'https://github-searchapp.vercel.app/',
-    codeURL: 'https://github.com/TiagoJacintoDev/github-search-api',
+    demoURL: "https://github-searchapp.vercel.app/",
+    codeURL: "https://github.com/TiagoJacintoDev/github-search-api",
     img: GitHubSearch,
   },
   {
-    title: 'Rent a Car Landing Page',
+    title: "CV Maker",
     skills: [
       {
-        text: 'React',
+        text: "React",
         img: <SiReact />,
       },
-      {
-        text: 'JavaScript',
-        img: <SiJavascript />,
-      },
-      {
-        text: 'styled-components',
-        img: <SiStyledcomponents />,
-      },
-      {
-        text: 'Framer Motion',
-        img: <SiFramer />,
-      },
+      { text: "JavaScript", img: <SiJavascript /> },
+      { text: "CSS Modules", img: <SiCss3 /> },
     ],
     description:
-      'A landing page using the Eleanor Auto website design. This was my first time working with animations. My main challenge here was managing exit animations & positioning images depending on the media screen. Other than that I managed to improve on file structure & defining props with objects',
-    demoURL: 'https://rent-a-car-landing-page-tiagojacintodev.vercel.app/',
-    codeURL: 'https://github.com/TiagoJacintoDev/rent-a-car-landing-page',
-    img: RentACarLandingPage,
+      "A CV Maker app that updates/changes the CV when you type your information out. With this project I've worked mostly with a large state management and controlled inputs.",
+    demoURL: "https://cv-makerapp.vercel.app/",
+    codeURL: "https://github.com/TiagoJacintoDev/cv-maker",
+    img: CVMaker,
   },
-  {
-    title: 'Dog Wiki',
-    skills: [
-      {
-        text: 'React',
-        img: <SiReact />,
-      },
-      {
-        text: 'JavaScript',
-        img: <SiJavascript />,
-      },
-      {
-        text: 'React Query',
-      },
-      {
-        text: 'styled-components',
-        img: <SiStyledcomponents />,
-      },
+  // {
+  //   title: 'Travel Landing Page',
+  //   skills: [
+  //     {
+  //       text: 'React',
+  //       img: <SiReact />,
+  //     },
+  //     {
+  //       text: 'JavaScript',
+  //       img: <SiJavascript />,
+  //     },
+  //     {
+  //       text: 'styled-components',
+  //       img: <SiStyledcomponents />,
+  //     },
+  //   ],
+  //   description:
+  //     'This project is a travel agency landing page with a mobile first design. My main focus for this project was to have a visually appealing page with responsive functionality. ',
+  //   demoURL: 'https://travel-landing-pageapp.vercel.app/',
+  //   codeURL: 'https://github.com/TiagoJacintoDev/travel-landing-page',
+  //   img: TravelLandingPage,
+  // },
+  // {
+  //   title: 'Rent a Car Landing Page',
+  //   skills: [
+  //     {
+  //       text: 'React',
+  //       img: <SiReact />,
+  //     },
+  //     {
+  //       text: 'JavaScript',
+  //       img: <SiJavascript />,
+  //     },
+  //     {
+  //       text: 'styled-components',
+  //       img: <SiStyledcomponents />,
+  //     },
+  //     {
+  //       text: 'Framer Motion',
+  //       img: <SiFramer />,
+  //     },
+  //   ],
+  //   description:
+  //     'A landing page using the Eleanor Auto website design. This was my first time working with animations. My main challenge here was managing exit animations & positioning images depending on the media screen. Other than that I managed to improve on file structure & defining props with objects',
+  //   demoURL: 'https://rent-a-car-landing-page-tiagojacintodev.vercel.app/',
+  //   codeURL: 'https://github.com/TiagoJacintoDev/rent-a-car-landing-page',
+  //   img: RentACarLandingPage,
+  // },
+  // {
+  //   title: 'Dog Wiki',
+  //   skills: [
+  //     {
+  //       text: 'React',
+  //       img: <SiReact />,
+  //     },
+  //     {
+  //       text: 'JavaScript',
+  //       img: <SiJavascript />,
+  //     },
+  //     {
+  //       text: 'React Query',
+  //     },
+  //     {
+  //       text: 'styled-components',
+  //       img: <SiStyledcomponents />,
+  //     },
 
-      {
-        text: 'Axios',
-      },
-    ],
-    description:
-      "In this application you can search for different dog breeds and get to know about them. I've used this API to fetch data from.",
-    demoURL: 'https://dog-wiki-app.vercel.app/',
-    codeURL: 'https://github.com/TiagoJacintoDev/dog-wiki',
-    img: DogWiki,
-  },
-  {
-    title: 'Ecommerce',
-    skills: [
-      {
-        text: 'React',
-        img: <SiReact />,
-      },
-      {
-        text: 'Javascript',
-        img: <SiJavascript />,
-      },
-      {
-        text: 'Firebase',
-        img: <SiFirebase />,
-      },
-      {
-        text: 'Tailwind CSS',
-        img: <SiTailwindcss />,
-      },
-      {
-        text: 'React Query',
-      },
-      {
-        text: 'Axios',
-      },
-    ],
-    description:
-      'This application was inspired in the website KuantoKusta. This was a large project that taught me what are the pain points of building and scaling a large app.',
-    demoURL: 'https://ecommerce-tiago.vercel.app/',
-    codeURL: 'https://github.com/TiagoJacintoDev/ecommerce',
-    img: EcommerceApp,
-  },
+  //     {
+  //       text: 'Axios',
+  //     },
+  //   ],
+  //   description:
+  //     "In this application you can search for different dog breeds and get to know about them. I've used this API to fetch data from.",
+  //   demoURL: 'https://dog-wiki-app.vercel.app/',
+  //   codeURL: 'https://github.com/TiagoJacintoDev/dog-wiki',
+  //   img: DogWiki,
+  // },
 ];
--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,46 +1,54 @@
-import CSharp from '../images/c-sharp-c.svg';
-import JavaScript from '../images/icons8-javascript.svg';
-import CSS from '../images/icons8-css3.svg';
-import HTML from '../images/icons8-html-5.svg';
-import Git from '../images/icons8-git.svg';
-import GitHub from '../images/icons8-github.svg';
-import Figma from '../images/icons8-figma.svg';
-import Linux from '../images/linux.png';
-import npm from '../images/icons8-npm.svg';
-import Unity from '../images/icons8-unity.svg';
-import React from '../images/icons8-react-native.svg';
-import Tailwind from '../images/icons8-tailwind-css.svg';
-import Firebase from '../images/icons8-firebase.svg';
-import SASS from '../images/icons8-sass.svg';
+import CSharp from "../images/c-sharp-c.svg";
+import JavaScript from "../images/icons8-javascript.svg";
+import TypesScript from "../images/icons8-typescript.svg";
+import CSS from "../images/icons8-css3.svg";
+import HTML from "../images/icons8-html-5.svg";
+import Git from "../images/icons8-git.svg";
+import GitHub from "../images/icons8-github.svg";
+import Figma from "../images/icons8-figma.svg";
+import Linux from "../images/linux.png";
+import npm from "../images/icons8-npm.svg";
+import Unity from "../images/icons8-unity.svg";
+import React from "../images/icons8-react-native.svg";
+import Tailwind from "../images/icons8-tailwind-css.svg";
+import Firebase from "../images/icons8-firebase.svg";
+import SASS from "../images/icons8-sass.svg";
+import Jest from "../images/jest-logo-svg-vector.svg";
+import ReactTesting from "../images/react-testing-library.png";
+import ReactRouter from "../images/react-router-seeklogo.com.svg";
+import ReactQuery from "../images/react-query.svg";
 
 export const skillsData = [
   {
-    type: 'Languages',
+    type: "Languages",
     list: [
-      { text: 'HTML', img: HTML },
-      { text: 'CSS', img: CSS },
-      { text: 'JavaScript', img: JavaScript },
-      { text: 'C#', img: CSharp },
-      { text: 'Sass', img: SASS },
+      { text: "HTML", img: HTML },
+      { text: "CSS", img: CSS },
+      { text: "JavaScript", img: JavaScript },
+      { text: "TypesScript", img: TypesScript },
+      { text: "C#", img: CSharp },
     ],
   },
   {
-    type: 'Frameworks/Libraries',
+    type: "Frameworks/Libraries",
     list: [
-      { text: 'React', img: React },
-      { text: 'Tailwind', img: Tailwind },
-      { text: 'Firebase', img: Firebase },
-      { text: 'Unity', img: Unity },
+      { text: "React", img: React },
+      { text: "Sass", img: SASS },
+      { text: "Tailwind", img: Tailwind },
+      { text: "Jest", img: Jest },
+      { text: "React Testing", img: ReactTesting },
+      { text: "React Query", img: ReactQuery },
+      { text: "React Router", img: ReactRouter },
     ],
   },
   {
-    type: 'Technologies',
+    type: "Technologies",
     list: [
-      { text: 'Git', img: Git },
-      { text: 'GitHub', img: GitHub },
-      { text: 'npm', img: npm },
-      { text: 'Figma', img: Figma },
-      { text: 'Linux', img: Linux },
+      { text: "Git", img: Git },
+      { text: "GitHub", img: GitHub },
+      { text: "npm", img: npm },
+      { text: "Figma", img: Figma },
+      { text: "Linux", img: Linux },
     ],
   },
 ];
