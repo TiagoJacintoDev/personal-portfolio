f26f93f050d84abe4edb10f09f87050969ceb3aa
tiagojacintodev <tiagojacintodev@gmail.com>
2023-02-02 14:50:59 +0000

    Add projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,10 +4,14 @@ import {
   SiCss3,
   SiTailwindcss,
   SiFirebase,
+  SiTypescript,
+  SiPrettier,
 } from "react-icons/si";
 import CVMaker from "../images/cv-maker.png";
 import GitHubSearch from "../images/github-search.png";
 import EcommerceApp from "../images/ecommerce.png";
+import GPT3 from "../images/gpt3.png";
+import WeatherApp from "../images/weather-app.png";
 
 export const projectsData = [
   {
@@ -43,6 +47,73 @@ export const projectsData = [
     img: EcommerceApp,
   },
   {
+    title: "GPT-3 Landing Page",
+    skills: [
+      {
+        text: "React",
+        img: <SiReact />,
+      },
+      {
+        text: "TypeScript",
+        img: <SiTypescript />,
+      },
+      {
+        text: "Tailwind CSS",
+        img: <SiTailwindcss />,
+      },
+      {
+        text: "Prettier",
+        img: <SiPrettier />,
+      },
+    ],
+    description:
+      "This landing page was made thinking of a mobile first design. My main goal in this project was to have a visually appealing page with responsive functionality.",
+    demoURL: "https://gpt3-landing-page-app.vercel.app/",
+    codeURL: "https://github.com/TiagoJacintoDev/gpt3-landing-page",
+    img: GPT3,
+  },
+  {
+    title: "CV Maker",
+    skills: [
+      {
+        text: "React",
+        img: <SiReact />,
+      },
+      { text: "JavaScript", img: <SiJavascript /> },
+      { text: "CSS Modules", img: <SiCss3 /> },
+    ],
+    description:
+      "A CV Maker app that updates/changes the CV when you type your information out. With this project I've worked mostly with a large state management and controlled inputs.",
+    demoURL: "https://cv-makerapp.vercel.app/",
+    codeURL: "https://github.com/TiagoJacintoDev/cv-maker",
+    img: CVMaker,
+  },
+  {
+    title: "Weather App",
+    skills: [
+      {
+        text: "React",
+        img: <SiReact />,
+      },
+      {
+        text: "TypeScript",
+        img: <SiTypescript />,
+      },
+      {
+        text: "Tailwind CSS",
+        img: <SiTailwindcss />,
+      },
+      {
+        text: "Axios",
+      },
+    ],
+    description:
+      "In this application you can search for different cities to know the their weather. I've used the OpenWeather API to fetch data from.",
+    demoURL: "https://weather-app-pt.vercel.app/",
+    codeURL: "https://github.com/TiagoJacintoDev/weather-app",
+    img: WeatherApp,
+  },
+  {
     title: "GitHub Search",
     skills: [
       {
@@ -64,97 +135,4 @@ export const projectsData = [
     codeURL: "https://github.com/TiagoJacintoDev/github-search-api",
     img: GitHubSearch,
   },
-  {
-    title: "CV Maker",
-    skills: [
-      {
-        text: "React",
-        img: <SiReact />,
-      },
-      { text: "JavaScript", img: <SiJavascript /> },
-      { text: "CSS Modules", img: <SiCss3 /> },
-    ],
-    description:
-      "A CV Maker app that updates/changes the CV when you type your information out. With this project I've worked mostly with a large state management and controlled inputs.",
-    demoURL: "https://cv-makerapp.vercel.app/",
-    codeURL: "https://github.com/TiagoJacintoDev/cv-maker",
-    img: CVMaker,
-  },
-  // {
-  //   title: 'Travel Landing Page',
-  //   skills: [
-  //     {
-  //       text: 'React',
-  //       img: <SiReact />,
-  //     },
-  //     {
-  //       text: 'JavaScript',
-  //       img: <SiJavascript />,
-  //     },
-  //     {
-  //       text: 'styled-components',
-  //       img: <SiStyledcomponents />,
-  //     },
-  //   ],
-  //   description:
-  //     'This project is a travel agency landing page with a mobile first design. My main focus for this project was to have a visually appealing page with responsive functionality. ',
-  //   demoURL: 'https://travel-landing-pageapp.vercel.app/',
-  //   codeURL: 'https://github.com/TiagoJacintoDev/travel-landing-page',
-  //   img: TravelLandingPage,
-  // },
-  // {
-  //   title: 'Rent a Car Landing Page',
-  //   skills: [
-  //     {
-  //       text: 'React',
-  //       img: <SiReact />,
-  //     },
-  //     {
-  //       text: 'JavaScript',
-  //       img: <SiJavascript />,
-  //     },
-  //     {
-  //       text: 'styled-components',
-  //       img: <SiStyledcomponents />,
-  //     },
-  //     {
-  //       text: 'Framer Motion',
-  //       img: <SiFramer />,
-  //     },
-  //   ],
-  //   description:
-  //     'A landing page using the Eleanor Auto website design. This was my first time working with animations. My main challenge here was managing exit animations & positioning images depending on the media screen. Other than that I managed to improve on file structure & defining props with objects',
-  //   demoURL: 'https://rent-a-car-landing-page-tiagojacintodev.vercel.app/',
-  //   codeURL: 'https://github.com/TiagoJacintoDev/rent-a-car-landing-page',
-  //   img: RentACarLandingPage,
-  // },
-  // {
-  //   title: 'Dog Wiki',
-  //   skills: [
-  //     {
-  //       text: 'React',
-  //       img: <SiReact />,
-  //     },
-  //     {
-  //       text: 'JavaScript',
-  //       img: <SiJavascript />,
-  //     },
-  //     {
-  //       text: 'React Query',
-  //     },
-  //     {
-  //       text: 'styled-components',
-  //       img: <SiStyledcomponents />,
-  //     },
-
-  //     {
-  //       text: 'Axios',
-  //     },
-  //   ],
-  //   description:
-  //     "In this application you can search for different dog breeds and get to know about them. I've used this API to fetch data from.",
-  //   demoURL: 'https://dog-wiki-app.vercel.app/',
-  //   codeURL: 'https://github.com/TiagoJacintoDev/dog-wiki',
-  //   img: DogWiki,
-  // },
 ];
